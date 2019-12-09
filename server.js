37fee3a52fd515971dcbdcad8d2a248b38492a97
Connor McDonald <connormcdxc@gmail.com>
2019-12-09 16:53:00 -0500

    Updates

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const fetch = require('node-fetch');
+const express = require("express");
+const fetch = require("node-fetch");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -35,38 +35,189 @@ app.use(express.json());
  *  - Express: app.use()
  *    https://expressjs.com/en/4x/api.html#app.use
  */
-app.use(express.static('src'));
+app.use(express.static("src"));
 
 // this is a single route, in the simplest possible format
 // the simplest format is not necessarily the best one.
 // this is, right now, an introduction to Callback Hell
-// but it is okay for a first-level example
-app.get('/api', (req, res) => {
-  const baseURL = 'https://data.princegeorgescountymd.gov/resource/9tsa-iner.json';
+// but it is okay for a first-level example 
+app.get("/api/year/:year/district/:district", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/9tsa-iner.json";
   fetch(baseURL)
-    .then((res) => res.json())
+    .then(res => res.json())
     .then(data => {
-        //console.log(data);
-        // FILTERING 
-        /*layerGroup.clearLayers();
-        if(value) {
-          res = res.filter(res => res.fiscalyear === value);
-        }*/
-        //mymap.addLayer(layerGroup);
-        //return res;
-        //data = data.filter((data) => data.fiscalyear=== 'FY2017');
-        const array = {};
-        for (let i = 0; i < data.length; i++) {
-          //array.push(data[i]);
-        }
+      let year = req.params.year;
+      //console.log("year: " + year);
+      let district = req.params.district;
+      console.log("district: " + district);
+      if(year !== "" && district !== "") {
+        data = data.filter(data => data.council_district === district);
+        console.log(data);
+        data = data.filter(data => data.fiscalyear === year);
+        console.log(data);
         
-        console.log(array);
-        res.send({ data });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.redirect('/error');
-      });
+        //console.log("filtering 2");
+      } 
+      res.send({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
     });
-
+});
+app.get("/api/FY2017", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/9tsa-iner.json";
+  fetch(baseURL)
+    .then(res => res.json())
+    .then(data => {
+      data = data.filter(data => data.fiscalyear === "FY2017");
+      res.send({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
+    });
+});
+app.get("/api/FY2018", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/9tsa-iner.json";
+  fetch(baseURL)
+    .then(res => res.json())
+    .then(data => {
+      data = data.filter(data => data.fiscalyear === "FY2018");
+      res.send({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
+    });
+});
+app.get("/api/1", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/9tsa-iner.json";
+  fetch(baseURL)
+    .then(res => res.json())
+    .then(data => {
+      data = data.filter(data => data.council_district === "1");
+      res.send({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
+    });
+});
+app.get("/api/2", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/9tsa-iner.json";
+  fetch(baseURL)
+    .then(res => res.json())
+    .then(data => {
+      data = data.filter(data => data.council_district === "2");
+      res.send({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
+    });
+});
+app.get("/api/3", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/9tsa-iner.json";
+  fetch(baseURL)
+    .then(res => res.json())
+    .then(data => {
+      data = data.filter(data => data.council_district === "3");
+      res.send({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
+    });
+});
+app.get("/api/4", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/9tsa-iner.json";
+  fetch(baseURL)
+    .then(res => res.json())
+    .then(data => {
+      data = data.filter(data => data.council_district === "4");
+      res.send({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
+    });
+});
+app.get("/api/5", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/9tsa-iner.json";
+  fetch(baseURL)
+    .then(res => res.json())
+    .then(data => {
+      data = data.filter(data => data.council_district === "5");
+      res.send({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
+    });
+});
+app.get("/api/6", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/9tsa-iner.json";
+  fetch(baseURL)
+    .then(res => res.json())
+    .then(data => {
+      data = data.filter(data => data.council_district === "6");
+      res.send({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
+    });
+});
+app.get("/api/7", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/9tsa-iner.json";
+  fetch(baseURL)
+    .then(res => res.json())
+    .then(data => {
+      data = data.filter(data => data.council_district === "7");
+      res.send({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
+    });
+});
+app.get("/api/8", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/9tsa-iner.json";
+  fetch(baseURL)
+    .then(res => res.json())
+    .then(data => {
+      data = data.filter(data => data.council_district === "8");
+      res.send({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
+    });
+});
+app.get("/api/9", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/9tsa-iner.json";
+  fetch(baseURL)
+    .then(res => res.json())
+    .then(data => {
+      data = data.filter(data => data.council_district === "9");
+      res.send({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
+    });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
